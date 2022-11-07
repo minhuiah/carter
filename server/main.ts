36492e04917a547e3232d742493a430f7dc11620
Kevin Chu <kevin@19hours.net>
2022-11-07 17:54:31 +0800

    Add rental cars

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
 import fetch from "node-fetch-commonjs";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+
 app.get("/rental", async (_req: Request, res: Response) => {
   let response = await fetch("https://api.bluesg.com.sg/oauth2/token/", {
     headers: {
@@ -25,8 +28,36 @@ app.get("/rental", async (_req: Request, res: Response) => {
     body: "grant_type=client_credentials",
     method: "POST",
   });
-  const data = await response.json();
-  return res.json(data);
+  const data = (await response.json()) as {
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+  };
+
+  let cars = await fetch("https://api.bluesg.com.sg/v2/station/?filter=cars", {
+    headers: {
+      accept: "application/json, text/plain, */*",
+      "accept-language": "en-GB,en;q=0.9",
+      authorization: `bearer ${data.access_token}`,
+      "cache-control": "no-cache",
+      pragma: "no-cache",
+      "sec-ch-ua":
+        '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"macOS"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-site",
+      cookie:
+        "_ga_N2P8DEFNKD=GS1.1.1667811855.1.0.1667811855.0.0.0; _ga=GA1.1.1446382805.1667811856",
+    },
+    referrerPolicy: "same-origin",
+    body: null,
+    method: "GET",
+  });
+
+  const carData = await cars.json();
+  return res.json(carData);
 });
 
 app.listen(3000, () => {
--- src/views/Rental.vue
+++ src/views/Rental.vue
@@ -1,3 +1,33 @@
 <template>
-  <div>REntral</div>
+  <mapbox-map
+    class="map"
+    mapStyle="mapbox://styles/mapbox/outdoors-v10?optimize=true"
+    :zoom="16"
+    accessToken="pk.eyJ1IjoiMWhpdW9uaiIsImEiOiJjbDlhcnNha2MwbWRtM3BxdDJ1d2psNTF5In0.3I_UAtOyTVSwLev2yEua8w"
+    :center="[location.lng, location.lat]"
+  >
+    <mapbox-geolocate-control />
+    <mapbox-navigation-control position="bottom-left" />
+    <mapbox-marker v-for="car in rentals" :lngLat="[car.lng, car.lat]">
+      <template v-slot:icon>
+        <div class="carpark-icon">
+          {{ car.rental_status }}
+        </div>
+      </template>
+    </mapbox-marker>
+  </mapbox-map>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapState } from "pinia";
+import { useMapStore } from "@/stores/Map";
+import { useRentalStore } from "@/stores/Rental";
+
+export default defineComponent({
+  computed: {
+    ...mapState(useRentalStore, ["rentals"]),
+    ...mapState(useMapStore, ["location"]),
+  },
+});
+</script>
